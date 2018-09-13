c72558cbc1e188581083f44b8725bbcef0c1c1ca
onelong <love@onelong.org>
2018-09-14 03:21:58 +0800

    chore: release 0.0.2

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -3,7 +3,4 @@
 SETLOCAL
 SET PATH = node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-::config
-SET ADMINER_EMAIL=youremail@example.com
-
-node_modules/.bin/hubot -a wechat-better --name bot %*
+node_modules/.bin/hubot -a wechat-better --name wechat-bot %*
